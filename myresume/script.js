0e79fbfefd9507bf90d877500954f0284175edb6
manivannan <manisundar9194@gmail.com>
2023-01-20 15:38:15 +0530

    resume

---
+++ myresume/script.js
@@ -0,0 +1,45 @@
+let myResume={
+    "basics": {
+      "name": "Manivannan Kalyanasundaram",
+      "email": "manisundar9194@gmail.com",
+      "phone": 6380747824,
+      "degree": "B.E",
+      "location": {
+        "address": "86,pallivasal steet ,seepalakottai",
+        "postalCode": 625540,
+        "city": "Theni",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      
+    "education": [
+      {
+        "institution": "Imayam College Of Engineering",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2011,
+        "batch end year": 2015,
+        "gpa": 6.3,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using python language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
